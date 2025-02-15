dd5a078c1da23b61452a2bd4289e9f845e71ae0a
Rowland <rowlandelegbede@gmail.com>
2025-02-15 15:34:42 +0100

    Added files

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'bookshelf.apps.BookshelfConfig',
 ]
 
 ROOT_URLCONF = 'LibraryProject.urls'
