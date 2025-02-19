2db76e74744c268904e74cda3a3d3d2695562408
Rowland <rowlandelegbede@gmail.com>
2025-02-19 19:39:24 +0100

    django-models

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,24 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def books_by_specific_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)
+        return books
+    except Author.DoesNotExist:
+        return f"{author_name} not found"
+    
+def list_of_books(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        return books
+    except Library.DoesNotExist:
+        return f"{library_name} not found"
+def librarian_for_library(librarian_name):
+    try:
+        library = Librarian.objects.get(name=librarian_name)
+        librarian = library.librarian
+        return librarian
+    except Library.DoesNotExist:
+        return f"{librarian_name} not found"
