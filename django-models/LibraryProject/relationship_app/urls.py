9425790d0891f294347b1cfe69ecd51786e28e3b
Rowland <rowlandelegbede@gmail.com>
2025-02-21 17:49:33 +0100

    Templates Added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import LibraryDetailView, list_of_books
+from .views import LibraryDetailView
+from .views import list_of_books
 urlpatterns = [
     path('books/', list_of_books, name='book-list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
