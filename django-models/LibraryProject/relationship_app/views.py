55f965a6000a8943163f0576154e87319ba3faf3
Rowland <rowlandelegbede@gmail.com>
2025-02-22 16:31:22 +0100

    Authenticator added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+
 # Create your views here.
 
 def register(request):
