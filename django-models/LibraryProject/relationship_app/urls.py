32ac1a3c233b18f50a5bd27f1fe2bdca1a12e780
Rowland <rowlandelegbede@gmail.com>
2025-02-22 16:28:16 +0100

    Authenticator added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import LibraryDetailView
 from .views import list_books
+from .views import register, user_login, user_logout
 urlpatterns = [
+    path("register/", register, name="register"),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
     path('books/', list_books, name='book-list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.succcess(request, 'Registrationsuccessful!')
+            return redirect('book-list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+def user_login(request):
+    request.method == 'POST'
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful!")
+            return redirect("book-list")
+    else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, "relationship_app/login.html")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("login")
+
 def list_books(request):
     books = Book.objects.all()
-    context = {
-        "books": books
-    }
-
+    context = {"books": books}
     return render(request, "relationship_app/list_books.html", context)
 
 class LibraryDetailView(DetailView):
