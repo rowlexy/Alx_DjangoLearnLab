e63631a4c2005ee0ad74f6b22738fec396cb554c
Rowland <rowlandelegbede@gmail.com>
2025-02-22 16:36:31 +0100

    Authenticator added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .views import LibraryDetailView
 from .views import list_books
 from .views import register, user_login, user_logout
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogOutView
 urlpatterns = [
+    path("views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="),
     path("register/", register, name="register"),
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
